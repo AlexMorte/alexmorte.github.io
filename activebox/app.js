1118a2276981ce58161bb1c8a0c541cf3d0cc3b0
AlexMorte <alexpobeditel1999@gmail.com>
2021-03-22 22:05:28 +0200

    Second Site

---
+++ activebox/app.js
@@ -0,0 +1,27 @@
+$(function() {
+
+	/* Fixed Header */
+
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH = intro.innerHeight();
+	let scrollPos = $(window).scrollTop();
+
+	$(window).on("scroll load resize", function() {
+
+		let introH = intro.innerHeight();
+		scrollPos = $(this).scrollTop();
+
+
+		if( scrollPos > introH ) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed");
+		}
+
+	});
+
+	/* Smooth Scroll */
+
+
+});
